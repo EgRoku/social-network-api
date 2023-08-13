97107436627c5fee376382d4cda152beaf880741
EgRoku <jimenezjj5150@gmail.com>
2023-08-13 12:21:24 -0700

    added routes for thoughts and users as well as associated index.js files. Created controller files for logic of routes

---
+++ routes/api/thought-routes.js
@@ -0,0 +1,28 @@
+const router = require("express").Router();
+const {
+  getThoughts,
+  createThought,
+  getSingleThought,
+  updateThought,
+  deleteThought,
+  addReaction,
+  removeReaction,
+} = require("../../controllers/thought-controller");
+
+// route for getThoughts and create new thought  /api/thoughts
+router.route('/').get(getThoughts).post(createThought);
+
+// route for thoughts by id with update and delete options  /api/thoughts/:thoughtId
+router
+    .route('/:thoughtId')
+    .get(getSingleThought)
+    .put(updateThought)
+    .delete(deleteThought);
+
+// route for adding reactions to thoughts  /api/thoughts/:thoughtId/reactions
+router.route('/:thoughtsId/reactions').post(addReaction);
+
+// route for deleting reactions by id  /api/thoughts/:thoughtId/reactions/:reactionId
+router.route('/api/thoughts/:thoughtId/reactions/:reactionId').delete(removeReaction);
+
+module.exports = router;
---
+++ routes/api/user-routes.js
@@ -0,0 +1,19 @@
+const router = require("express").Router();
+const {
+  getUsers,
+  createUser,
+  getSingleUser,
+  updateUser,
+  deleteUser,
+  addFriend,
+  removeFriend,
+} = require("../../controllers/user-controller");
+
+// route for getting users and creating them  /api/users
+router.route('/').get(getUsers).post(createUser);
+
+// route for getting user by id with update and delete options  /api/users/:userId
+router.route('/api/users/:userId').get(getSingleUser).put(updateUser).delete(deleteUser);
+
+// route for getting friends by id as well as the add and delete friend options  /:userId/friends/:friendId
+router.route('/:userId/friends/:friendId').post(addFriend).delete(removeFriend);
